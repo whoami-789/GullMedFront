f425039bb8639b54bb33d6808a7819ddaa069a77
whoami-789 <tudya1238@gmail.com>
2024-04-24 11:32:38 +0500

    Procedures

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const Footer: React.FC = () => {
+    const {t} = useTranslation();
+
     return (
         <footer className="w-full bg-blue-700 text-white text-center py-4 mb-0 m-0">
             <div className="container mx-auto text-center text-white mb-0 m-0">
-                <a>&copy; 2024 Gull Med. Все права защищены.</a>
+                <a>{t('footer')}</a>
                 <br/>
             </div>
         </footer>
